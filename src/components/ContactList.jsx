4478a8c34f07eacad23a1084d03b2f4251a2d626
Meredith Butterworth <meredithbutterworth@gmail.com>
2024-02-12 20:19:24 -0500

    catch error updated

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -24,6 +24,7 @@ export default function ContactList() {
               setContacts(json);
               // console.log(contacts);           
           } catch (error) {
+            console.log(error);
           }
         }
     fetchContacts()
